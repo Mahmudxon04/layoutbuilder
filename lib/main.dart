04a3771b3744e15a7d1fba3bf667d893f0a3b3db
Mahmudxon04 <mahmudxon0205@gmail.com>
2024-02-01 20:59:16 +0500

    Qorbobo

--- lib/main.dart
+++ lib/main.dart
@@ -13,60 +13,176 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+      home: Example2(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
 
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
+class Example2 extends StatelessWidget {
+  const Example2({Key? key}) : super(key: key);
 
-}
-
-class _MainScreenState extends State<MainScreen>
-  with SingleTickerProviderStateMixin {
-
-  late AnimationController animationController;
-  late Tween<double> animation;
-  @override
-  void initState() {
-  animationController = AnimationController(
-    vsync: this, duration: const Duration(seconds: 1),
-  )..addStatusListener((status) {
-    if(status == AnimationStatus.completed) {
-      animationController.reverse();
-    } else if(status == AnimationStatus.dismissed) {
-      animationController.forward();
-    }
-
-  });
-  animation = Tween<double>(
-    begin: 0.8, end: 0,
-  );
-
-  animationController.forward();
-  super.initState();
-
-
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child: AnimatedBuilder(
-          builder: (context, child) => CustomPaint(
-            painter: PackManPainter(
-              mouthExtend: animationController.value,
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Snowman'),
+      ),
+      body: CustomPaint(
+        painter: MyPainter(),
+        child: Container(),
+      ),
+    );
+  }
+}
 
-            ),
-          ),
-          animation: animation.animate(animationController),
-        ),
-      )
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5;
+    canvas.drawCircle(Offset(size.width / 2, size.height * 3.5 / 10), 70, Paint()..color = Colors.white);
+    canvas.drawCircle(Offset(size.width / 2, size.height * 5.5 / 10), 100, Paint()..color = Colors.white);
+    canvas.drawCircle(Offset(size.width / 2, size.height * 8 / 10), 120, Paint()..color = Colors.white);
+    paint.strokeWidth = 2;
+    // body
+    canvas.drawCircle(Offset(size.width * 5.2 / 10, size.height * 5 / 10), 5, Paint()..color = Colors.black);
+    canvas.drawCircle(Offset(size.width * 5.2 / 10, size.height * 5.3 / 10), 5, Paint()..color = Colors.black);
+    canvas.drawCircle(Offset(size.width * 5.2 / 10, size.height * 5.6 / 10), 5, Paint()..color = Colors.black);
+    // head
+    canvas.drawCircle(Offset(size.width * 5.8 / 10, size.height * 3.2 / 10), 5, Paint()..color = Colors.blue);
+    canvas.drawCircle(Offset(size.width * 4.6 / 10, size.height * 3.2 / 10), 5, Paint()..color = Colors.blue);
+    // smile
+    canvas.drawCircle(Offset(size.width * 4.6 / 10, size.height * 3.8 / 10), 5, Paint()..color = Colors.black);
+    canvas.drawCircle(Offset(size.width * 4.9 / 10, size.height * 3.9 / 10), 5, Paint()..color = Colors.black);
+    canvas.drawCircle(Offset(size.width * 5.3 / 10, size.height * 3.9 / 10), 5, Paint()..color = Colors.black);
+    canvas.drawCircle(Offset(size.width * 5.6 / 10, size.height * 3.8 / 10), 5, Paint()..color = Colors.black);
+    // nose
+    final nosePath = Path()
+      ..moveTo(size.width * 5.2 / 10, size.height * 3.4 / 10)
+      ..lineTo(size.width * 7.6 / 10, size.height * 3.3 / 10)
+      ..lineTo(size.width * 5.2 / 10, size.height * 3.5 / 10)
+      ..lineTo(size.width * 5.2 / 10, size.height * 3.4 / 10);
+    canvas.drawPath(nosePath, Paint()..color = Colors.red);
+    final leftArm = Path()
+      ..moveTo(size.width * 7.2 / 10, size.height * 4.8 / 10)
+      ..lineTo(size.width * 8.5 / 10, size.height * 4.3 / 10)
+      ..lineTo(size.width * 8.7 / 10, size.height * 4 / 10)
+      ..lineTo(size.width * 8.85 / 10, size.height * 4.05 / 10)
+      ..lineTo(size.width * 8.7 / 10, size.height * 4.3 / 10)
+      ..lineTo(size.width * 9 / 10, size.height * 4.15 / 10)
+      ..lineTo(size.width * 9.1 / 10, size.height * 4.2 / 10)
+      ..lineTo(size.width * 8.74 / 10, size.height * 4.38 / 10)
+      ..lineTo(size.width * 9.16 / 10, size.height * 4.46 / 10)
+      ..lineTo(size.width * 9.1 / 10, size.height * 4.55 / 10)
+      ..lineTo(size.width * 8.65 / 10, size.height * 4.46 / 10)
+      ..lineTo(size.width * 7.32 / 10, size.height * 4.97 / 10);
+    print('ok');
+    paint.strokeWidth = 5;
+    canvas.drawPath(leftArm, Paint()..color = Colors.brown);
+    final rightArm = Path()
+      ..moveTo(size.width * 2.7 / 10, size.height * 4.8 / 10)
+      ..lineTo(size.width * 1 / 10, size.height * 4 / 10)
+      ..lineTo(size.width * 0.9 / 10, size.height * 3.7 / 10)
+      ..lineTo(size.width * 0.7 / 10, size.height * 3.72 / 10)
+      ..lineTo(size.width * 0.78 / 10, size.height * 4.05 / 10)
+      ..lineTo(size.width * 0.38 / 10, size.height * 3.9 / 10)
+      ..lineTo(size.width * 0.3 / 10, size.height * 4 / 10)
+      ..lineTo(size.width * 0.77 / 10, size.height * 4.2 / 10)
+      ..lineTo(size.width * 0.35 / 10, size.height * 4.3 / 10)
+      ..lineTo(size.width * 0.35 / 10, size.height * 4.4 / 10)
+      ..lineTo(size.width * 0.9 / 10, size.height * 4.3 / 10)
+      ..lineTo(size.width * 2.6 / 10, size.height * 5.08 / 10);
+    // hat
+
+    print('ok');
+
+    canvas.drawPath(rightArm, Paint()..color = Colors.brown);
+    canvas.drawOval(
+        Rect.fromCenter(
+            center: Offset(size.width * 5 / 10, size.height * 2.6 / 10), width: 150, height: 40),
+        Paint()..color = Colors.black26);
+
+    final hatPath = Path()
+      ..moveTo(size.width * 4 / 10, size.height * 2.6 / 10)
+      ..lineTo(size.width * 4.1 / 10, size.height * 1.4 / 10)
+      ..lineTo(size.width * 6 / 10, size.height * 1.4 / 10)
+      ..lineTo(size.width * 6 / 10, size.height * 2.6 / 10)
+      ..lineTo(size.width * 4 / 10, size.height * 2.6 / 10);
+    canvas.drawPath(hatPath, Paint()..color = Colors.black26);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MainScreen extends StatefulWidget {
+//   const MainScreen({super.key});
+//
+//
+//   @override
+//   State<MainScreen> createState() => _MainScreenState();
+//
+// }
+//
+// class _MainScreenState extends State<MainScreen>
+//   with SingleTickerProviderStateMixin {
+//
+//   late AnimationController animationController;
+//   late Tween<double> animation;
+//   @override
+//   void initState() {
+//   animationController = AnimationController(
+//     vsync: this, duration: const Duration(seconds: 1),
+//   )..addStatusListener((status) {
+//     if(status == AnimationStatus.completed) {
+//       animationController.reverse();
+//     } else if(status == AnimationStatus.dismissed) {
+//       animationController.forward();
+//     }
+//
+//   });
+//   animation = Tween<double>(
+//     begin: 0.8, end: 0,
+//   );
+//
+//   animationController.forward();
+//   super.initState();
+//
+//
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body:Center(
+//         child: AnimatedBuilder(
+//           builder: (context, child) => CustomPaint(
+//             painter: PackManPainter(
+//               mouthExtend: animationController.value,
+//
+//             ),
+//           ),
+//           animation: animation.animate(animationController),
+//         ),
+//       )
       // LayoutBuilder(
       //   builder: (context, constraints) {
       //     print(constraints.maxHeight);
@@ -89,8 +205,8 @@ class _MainScreenState extends State<MainScreen>
       //     );
       //   },
       // ),
-    );
-  }
+  //   );
+  // }
   // int gatMaxColumnSize(double value) {
   //   if(value < 1020) {
   //     return 1;
@@ -100,7 +216,7 @@ class _MainScreenState extends State<MainScreen>
   //     return 3;
   //   }
   // }
-}
+// }
 
 // class WCard extends StatelessWidget {
 //   const WCard({
@@ -149,41 +265,41 @@ class _MainScreenState extends State<MainScreen>
 //   }
 // }
 
-class PackManPainter extends CustomPainter {
-
-  final double mouthExtend;
-  const PackManPainter({
-    super.repaint,
-    required this.mouthExtend,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final centerX = size.width / 2;
-    final centerY = size.height / 2;
-    final paint = Paint();
-    paint.color = Colors.red;
-
-    canvas.drawArc(
-      Rect.fromCenter(
-        center: Offset(centerX, centerY),
-        width: 200,
-        height: 200,
-      ),
-      0.4,
-      2 * pi - mouthExtend,
-      true,
-      paint,
-    );
-
-    canvas.drawCircle(
-      Offset(-20, -50),
-      10,
-      Paint()..color = Colors.black,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-}
+// class PackManPainter extends CustomPainter {
+//
+//   final double mouthExtend;
+//   const PackManPainter({
+//     super.repaint,
+//     required this.mouthExtend,
+//   });
+//
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final centerX = size.width / 2;
+//     final centerY = size.height / 2;
+//     final paint = Paint();
+//     paint.color = Colors.red;
+//
+//     canvas.drawArc(
+//       Rect.fromCenter(
+//         center: Offset(centerX, centerY),
+//         width: 200,
+//         height: 200,
+//       ),
+//       0.4,
+//       2 * pi - mouthExtend,
+//       true,
+//       paint,
+//     );
+//
+//     canvas.drawCircle(
+//       Offset(-20, -50),
+//       10,
+//       Paint()..color = Colors.black,
+//     );
+//   }
+//
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+// }
 
